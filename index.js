e3bb36c35a7034afb02d0dd2a5e1056bf52def32
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-08-25 21:35:29 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+    const seenNumbers = {};
+  
+    for (const number of array) {
+      // n steps
+      const complement = target - number;
+      if (complement in seenNumbers) return true;
+      seenNumbers[number] = true;
+    }
+    return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
